0bd4a23053da7acd09ca911139030a9b4941e5ed
Meowveloper <zwekhantaung22.tgi@gmail.com>
2024-10-12 20:03:06 +0700

    initialize

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Luhn Algorithm
+// --------------
+// 1. Double every second digit from right to left
+//    If doubled number is 2 digits, split them
+// 2. Add all single digits from step 1
+// 3. Add all odd numbered digits from right to left
+// 4. Sum results from steps 2 & 3
+// 5. If step 4 is divisible by 10, # is valid
+#include <iostream>
+
+int main ()
+{
+    return 0;
+}
